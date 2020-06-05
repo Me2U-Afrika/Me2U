cadc0d4d91e59f85f241a60924408f72f34f0b64
DOgechi <dogechi17@alustudent.com>
2020-06-05 14:16:50 +0200

    changes to address field

--- me2ushop/models.py
+++ me2ushop/models.py
@@ -142,8 +142,8 @@ class Order(models.Model):
 
 class Address(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    street_address = models.CharField(max_length=10)
-    apartment_address = models.CharField(max_length=10)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
     country = CountryField(multiple=False)
     zip = models.CharField(max_length=10)
     address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
