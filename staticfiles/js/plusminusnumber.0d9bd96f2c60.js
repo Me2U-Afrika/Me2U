bd6f5bca74b9714c1b7d854ec074ac4b41ab29f5
DOgechi <dogechi17@alustudent.com>
2020-09-19 01:13:03 +0200

    staticfiles

---
+++ staticfiles/js/plusminusnumber.0d9bd96f2c60.js
@@ -0,0 +1,35 @@
+$(function(){
+    $('.btn-number').click(function(e){
+        console.log('we here')
+        e.preventDefault();
+        fieldName = $(this).attr('data-field');
+        typeof =  $(this).attr('data-type');
+        var input = $("input[name='"+ fieldName +"']");
+        var currentVal = parseInt(input.val());
+        if (type == 'minus'){
+            if (currentVal > input.attr('min')){
+                input
+                    .val(currentVal - 1)
+                    .change();
+            }
+
+         if (parseInt(input.val())== input.attr('min')) {
+            $(this).attr('disabled', true);
+         }
+
+        } else if (type == 'plus'){
+            input
+                .val(currentVal + 1)
+                 .change();
+        }
+
+    });
+
+});
+
+// function statusBox(){
+//        jQuery('<div id="loading"> Loading...</div>')
+//        .prependTo("#main")
+//        .ajaxStart(function(){jQuery(this).show();})
+//        .ajaxStop(function(){jQuery(this).hide();})
+//    }
