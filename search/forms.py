f1717541f2cc5bf18cf937bf52a17326b0e270da
DOgechi <dogechi17@alustudent.com>
2021-02-03 03:20:50 +0200

    seller pages updates

--- search/forms.py
+++ search/forms.py
@@ -13,7 +13,6 @@ class SearchForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
         default_text = 'Search Products, Brands & Categories'
-        self.fields['q'].widget.attrs['value'] = default_text
         self.fields['q'].widget.attrs['onfocus'] = "if (this.value=='" + default_text + "')this.value = ''"
         self.fields['q'].widget.attrs['placeholder'] = default_text
 
