ba7b9380e0695ac7e09ae443f1d7fbdd4fb430c3
DOgechi <danielmakori0@gmail.com>
2021-04-22 23:05:19 +0200

    changes

--- payments/mpesaApi/views.py
+++ payments/mpesaApi/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser, AllowAny
 from payments.mpesaApi.serializers import LNMOnlineSerializer
 from rest_framework.generics import CreateAPIView
 
@@ -8,8 +9,7 @@ from payments.models import LNMOnline
 class LNMCallbackAPIView(CreateAPIView):
     queryset = LNMOnline.objects.all()
     serializer_class = LNMOnlineSerializer
-
-    # permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         print(request.data, "request data")
