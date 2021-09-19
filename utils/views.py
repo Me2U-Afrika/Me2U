1e47955c1428123e45530ede71be54f152b58169
Dogechi <danielmakori0@gmail.com>
2021-09-19 10:39:58 +0200

    cached detail view authenticated

--- utils/views.py
+++ utils/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.cache import cache
 from django.views.generic import DetailView
+
+
 # Create your views here.
 
 class CachedDetailView(DetailView):
@@ -8,10 +10,17 @@ class CachedDetailView(DetailView):
         return super(CachedDetailView, self).get_queryset().select_related()
 
     def get_object(self, queryset=None):
-        obj = cache.get('%s-%s' % (self.model.__name__.lower(), self.kwargs['slug']), None)
+        if self.request.user.is_authenticated:
+            obj = None
 
-        if not obj:
-            obj = super(CachedDetailView, self).get_object(queryset)
-            cache.set('%s-%s' % (self.model.__name__.lower(), self.kwargs['slug']), obj)
+            if not obj:
+                obj = super(CachedDetailView, self).get_object(queryset)
+                cache.set('%s-%s' % (self.model.__name__.lower(), self.kwargs['slug']), obj)
+        else:
+            obj = cache.get('%s-%s' % (self.model.__name__.lower(), self.kwargs['slug']), None)
+
+            if not obj:
+                obj = super(CachedDetailView, self).get_object(queryset)
+                cache.set('%s-%s' % (self.model.__name__.lower(), self.kwargs['slug']), obj)
 
         return obj
